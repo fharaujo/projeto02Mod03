9bac364e7353c412415063490ea8a33e8a82caf5
fharaujo <faraujodeveloper@hotmail.com>
2021-08-30 20:12:49 -0300

    refactory indexJs

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const ObjectId = mongodb.ObjectId;
   const app = express();
   app.use(express.json());
   const port = 3000;
-  const connectionString = `mongodb://localhost:27017/db_project_blue`
+  const connectionString = `mongodb://localhost:27017/db_project_blue`;
+
+  // fazendo a conexão direta com o banco
+  const options = {
+    useUnifiedTopology: true,
+  };
+  const cliente = await mongodb.MongoClient.connect(connectionString, options);
+  const db = cliente.db("db_project_blue");
+  const characters = db.collection("characters");
 
   app.get("/", (req, res) => {
     res.send({ info: "Olá" });
   });
-
 
   app.listen(port, () => {
     console.log(`Servidor rodando em: http://localhost:${port}`);
